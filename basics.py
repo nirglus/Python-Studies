15473b141092881708f2653e3c67e33a6eb827fe
nirglus <nirgluskin@gmail.com>
2023-09-23 12:34:14 +0300

    complete task

--- basics.py
+++ basics.py
@@ -186,3 +186,20 @@ if age < 18:
       print("20% discount")  
 else:
    print("Regular price")
+
+# -------- Task ---------
+# Complete the given code to display different messages to the patient based on the blood sugar levels
+# Glucose level is an input for this software
+glucose_level = int(input())
+
+# Display message if glucose level is less than 70
+if glucose_level < 70:
+  print("Low glucose level")
+
+# Display message if glucose level is greater than 140
+elif glucose_level > 140:
+  print("High glucose level")
+
+# Display message if none of the conditions above are met
+else:
+  print("Normal range")
