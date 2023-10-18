2cec53fdc0d09d840e18c31e825df92eb0d062ed
nirglus <nirgluskin@gmail.com>
2023-10-18 09:02:12 +0300

    tuple unpacking

--- basics.py
+++ basics.py
@@ -464,3 +464,23 @@ def check_name(name, contacts):
     return "Not Found"        
     
 print(check_name(input(), contacts))
+
+#----------- Tuple Unpacking -----------
+# Tuple unpacking allows you to assign each item in a collection to a variable.
+
+numbers = (1, 2, 3)
+a, b, c = numbers
+print(a)
+print(b)
+print(c)
+
+# This can be also used to swap variables by doing a, b = b, a , since b, 
+# a on the right hand side forms the tuple (b, a) which is then unpacked.
+
+# A variable that is prefaced with an asterisk (*) takes all values from the collection
+#  that are left over from the other variables.
+a, b, *c, d = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(a) # prints 1
+print(b) # prints 2
+print(c) # prints all values from 3 to 8
+print(d) # prints 9
