3c263d444ddefd444fbaad53658e86ec15b3d055
nirglus <nirgluskin@gmail.com>
2023-10-19 10:39:28 +0300

    add set remove

--- basics.py
+++ basics.py
@@ -501,6 +501,8 @@ nums.remove(3)
 print(nums)
 print(len(nums))
 #! Duplicate elements will automatically get removed from the set.
+# NOTE - True and 1 will count as the same element.
+# NOTE - False and 0 will count as the same element.
 # The len() function can be used to return the number of elements of a set.
 """
 Sets can be combined using mathematical operations.
@@ -516,6 +518,23 @@ print(first & second) #! prints {4 ,5, 6}
 print(first - second) #! prints {1, 2, 3}
 print(second - first) #! prints {7, 8, 9}
 print(first ^ second) #! prints {1, 2, 3, 7, 8, 9}
+
+# ------- Removing items in sets
+# To remove an item in a set, use the remove(), or the discard() method.
+# NOTE: If the item to remove does not exist, remove() will raise an error.
+# NOTE: If the item to remove does not exist, discard() will NOT raise an error.
+# You can also use the pop() method to remove an item, but this method will remove a random item, 
+# so you cannot be sure what item that gets removed.
+# The return value of the pop() method is the removed item.
+# The clear() method empties the set:
+thisset = {"apple", "banana", "cherry"}
+thisset.clear()
+print(thisset) # prints set()
+
+# The del keyword will delete the set completely:
+thisset = {"apple", "banana", "cherry"}
+del thisset
+print(thisset) # error: thisset is not defines
 
 #! ----------- List Comprehensions -----------
 # List comprehensions are a useful way of quickly creating lists whose contents obey a rule.
