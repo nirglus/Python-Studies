f123602901bff6157f02af3c5b50b80325bc8b77
nirglus <nirgluskin@gmail.com>
2023-10-19 14:25:36 +0300

    add map and filter

--- basics.py
+++ basics.py
@@ -638,4 +638,31 @@ result_lambda = add_lambda(3, 5)
 print(result)         # Output: 8
 print(result_lambda)  # Output: 8
 
+#! ----------- Map -----------
+# The built-in functions map and filter are very useful higher-order functions that operate
+# on lists (or similar objects called iterables).
 
+# The function map takes a function and an iterable as arguments, 
+# and returns a new iterable with the function applied to each argument.
+def add_five(x):
+  return x + 5
+
+nums = [11, 22, 33, 44, 55]
+result = list(map(add_five, nums))
+print(result) # prints [16, 27, 38, 49, 60]
+
+# We could have achieved the same result more easily by using lambda syntax.
+nums = [11, 22, 33, 44, 55]
+
+result = list(map(lambda x: x+5, nums))
+print(result)
+# To convert the result into a list, we used list explicitly.
+
+#! ----------- Filter -----------
+# The function filter filters an iterable by leaving only 
+# the items that match a condition (also called a predicate).
+
+nums = [11, 22, 33, 44, 55]
+res = list(filter(lambda x: x%2==0, nums))
+print(res) # prints [22, 44]
+# Like map, the result has to be explicitly converted to a list if you want to print it.
