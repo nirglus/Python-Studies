2a6c992ae9e6c131e4afbfbd866df46fec4c6d2e
nirglus <nirgluskin@gmail.com>
2023-10-22 11:15:43 +0300

    add decorators

--- basics.py
+++ basics.py
@@ -720,3 +720,37 @@ f = int(input())
 t = int(input())
 
 print(list(primeGenerator(f, t)))
+
+#! ----------- Decorators -----------
+# Decorators provide a way to modify functions using other functions.
+# This is ideal when you need to extend the functionality of functions that you don't want to modify.
+def decor(func):
+  def wrap():
+    print("============")
+    func()
+    print("============")
+  return wrap
+
+def print_text():
+  print("Hello world!")
+
+decorated = decor(print_text)
+decorated()
+#! We defined a function named decor that has a single parameter func. Inside decor, 
+# we defined a nested function named wrap. The wrap function will print a string, then call func(), 
+# and print another string. The decor function returns the wrap function as its result.
+#! We could say that the variable decorated is a decorated version of print_text -- it's print_text plus something.
+#! In fact, if we wrote a useful decorator we might want to replace print_text with the decorated version 
+# altogether so we always got our "plus something" version of print_text.
+# This is done by re-assigning the variable that contains our function:
+print_text = decor(print_text)
+print_text()
+
+# You can decorate a function by pre-pending the function definition with a decorator name and the "@" symbol.
+# This allows you to wrap any function with custom behavior.
+@decor
+def print_text():
+  print("Hello world!")
+print_text() # prints 'Hello world' with the decoration we set before.
+# In Python, you can apply multiple decorators to a single function, 
+# which gives you the flexibility to modify its behavior in various ways.
