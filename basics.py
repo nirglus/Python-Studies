55a6b9bd0ba57e59dd2d087a757af3ee90cd6dc8
nirglus <nirgluskin@gmail.com>
2023-10-23 20:19:04 +0300

    add recursion

--- basics.py
+++ basics.py
@@ -754,3 +754,46 @@ def print_text():
 print_text() # prints 'Hello world' with the decoration we set before.
 # In Python, you can apply multiple decorators to a single function, 
 # which gives you the flexibility to modify its behavior in various ways.
+
+#! ----------- Recursion -----------
+# Recursion is a very important concept in functional programming.
+# The fundamental part of recursion is self-reference -- functions calling themselves.
+# It is used to solve problems that can be broken up into easier sub-problems of the same type.
+
+""" A classic example of a function that is implemented recursively is the factorial function, 
+which finds the product of all positive integers below a specified number.
+For example, 5! (5 factorial) is 5 * 4 * 3 * 2 * 1 (120). 
+To implement this recursively, notice that 5! = 5 * 4!, 4! = 4 * 3!, 3! = 3 * 2!, and so on. 
+Generally, n! = n * (n-1)!. Furthermore, 1! = 1.
+This is known as the base case, as it can be calculated without performing any more factorials.
+Below is a recursive implementation of the factorial function."""
+def factorial(x):
+  if x == 1:
+    return 1
+  else: 
+    return x * factorial(x-1)
+    
+print(factorial(4)) # prints 24
+#! The base case acts as the exit condition of the recursion.
+#! Not adding a base case results in infinite function calls, crashing the program.
+
+# Recursion can also be indirect. 
+# One function can call a second, which calls the first, which calls the second, and so on. 
+# This can occur with any number of functions.
+def is_even(x):
+  if x == 0:
+    return True
+  else:
+    return is_odd(x-1)
+
+def is_odd(x):
+  return not is_even(x)
+
+print(is_odd(9)) # prints True
+print(is_even(25)) # prints False
+def fib(x):
+ if x == 0 or x == 1:
+  return 1
+ else:
+  return fib(x-1) + fib(x-2)
+print(fib(4))
