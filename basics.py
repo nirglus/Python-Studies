4edb63a0fcfe125848fadc4f8b17c7c4489d9ba3
nirglus <nirgluskin@gmail.com>
2023-10-23 23:52:23 +0300

    add *args

--- basics.py
+++ basics.py
@@ -815,3 +815,15 @@ def convert(num):
      return (num % 2 + 10 * convert(num // 2)) 
 
 print(convert(int(input())))
+
+#! ----------- *args -----------
+# Python allows you to have functions with varying numbers of arguments.
+# Using *args as a function parameter enables you to pass an arbitrary number of arguments to that function. 
+# The arguments are then accessible as the tuple args in the body of the function.
+def function(named_arg, *args):
+   print(named_arg)
+   print(args)
+
+function(1, 2, 3, 4, 5) # prints 1 and then (2, 3, 4, 5)
+# The parameter *args must come after the named parameters to a function.
+# The name args is just a convention; you can choose to use another.
