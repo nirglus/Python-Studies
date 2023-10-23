494d62e724d9170ca45a5315cc884c8e0549ca2e
nirglus <nirgluskin@gmail.com>
2023-10-23 23:56:05 +0300

    add **kwargs

--- basics.py
+++ basics.py
@@ -816,7 +816,7 @@ def convert(num):
 
 print(convert(int(input())))
 
-#! ----------- *args -----------
+#! ----------- *args and **kwargs -----------
 # Python allows you to have functions with varying numbers of arguments.
 # Using *args as a function parameter enables you to pass an arbitrary number of arguments to that function. 
 # The arguments are then accessible as the tuple args in the body of the function.
@@ -827,3 +827,14 @@ def function(named_arg, *args):
 function(1, 2, 3, 4, 5) # prints 1 and then (2, 3, 4, 5)
 # The parameter *args must come after the named parameters to a function.
 # The name args is just a convention; you can choose to use another.
+
+# **kwargs (standing for keyword arguments) allows you to handle named arguments
+# that you have not defined in advance.
+# The keyword arguments return a dictionary in which the keys are the argument names,
+# and the values are the argument values.
+def my_func(x, y=7, *args, **kwargs):
+   print(kwargs)
+
+my_func(2, 3, 4, 5, 6, a=7, b=8) # prints {'a': 7, 'b': 8}
+# a and b are the names of the arguments that we passed to the function call.
+# The arguments returned by **kwargs are not included in *args.
