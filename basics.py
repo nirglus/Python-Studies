88676ad5e7209fc6770f46a1b5275ecc94a91692
nirglus <nirgluskin@gmail.com>
2023-10-26 16:08:13 +0300

    add task

--- basics.py
+++ basics.py
@@ -921,3 +921,20 @@ print(archik.name)
 print(archik.color)
 archik.bark()
 # NOTE Class attributes are shared by all instances of the class.
+
+# ----------- Task -----------
+"""
+You are making a video game! The given code declares a Player class, with its attributes and an intro() method.
+Complete the code to take the name and level from user input, 
+create a Player object with the corresponding values and call the intro() method of that object.
+"""
+class Player:
+    def __init__(self, name, level):
+        self.name = name
+        self.level = level
+
+    def intro(self):
+        print(self.name + " (Level " + self.level + ")")
+
+#your code goes here
+
