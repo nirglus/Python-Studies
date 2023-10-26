809700aca8538b5fc988b5a933383be04b129e86
nirglus <nirgluskin@gmail.com>
2023-10-26 16:29:05 +0300

    add inheritance

--- basics.py
+++ basics.py
@@ -938,7 +938,76 @@ class Player:
 
 #your code goes here
 name = input()
-2
+level = input()
 new_player = Player(name, level)
 new_player.intro()
+
+#! ----------- Inheritance -----------
+"""
+Inheritance provides a way to share functionality between classes.
+Imagine several classes, Cat, Dog, Rabbit and so on. 
+Although they may differ in some ways (only Dog might have the method bark), 
+they are likely to be similar in others (all having the attributes color and name).
+This similarity can be expressed by making them all inherit from a superclass Animal, which contains the shared functionality.
+To inherit a class from another class, put the superclass name in parentheses after the class name.
+"""
+class Animal: 
+  def __init__(self, name, color):
+    self.name = name
+    self.color = color
+
+class Cat(Animal):
+  def purr(self):
+    print("Purr...")
+        
+class Dog(Animal):
+  def bark(self):
+    print("Woof!")
+
+archik = Dog("Archik", "Black")
+print(archik.color)
+archik.bark() # prints "Woof!"
+archik.purr() # prints error
+thomas = Cat("Thomas", "Grey")
+print(thomas.color)
+thomas.bark() # prints error
+thomas.purr() # prints "Purr..."
+
+"""
+A class that inherits from another class is called a subclass.
+A class that is inherited from is called a superclass.
+If a class inherits from another with the same attributes or methods, it overrides them.
+"""
+class Wolf: 
+  def __init__(self, name, color):
+    self.name = name
+    self.color = color
+
+  def bark(self):
+    print("Awoooo")
+
+class Dog(Wolf):
+  def bark(self):
+    print("Woof!")
+        
+husky = Dog("Mika", "grey")
+husky.bark() # prints "Woof!"
+wolf = Wolf("Zeev", "white")
+wolf.bark() # prints "Awoooo"
+
+#NOTE In the example above, Wolf is the superclass, Dog is the subclass.
+
+# The function super is a useful inheritance-related function that refers to the parent class. 
+# It can be used to find the method with a certain name in an object's superclass.
+class A:
+  def spam(self):
+    print(1)
+
+class B(A):
+  def spam(self):
+    print(2)
+    super().spam()
+            
+B().spam() # prints 1, as the superclass A
+#NOTE super().spam() calls the spam method of the superclass.
 
