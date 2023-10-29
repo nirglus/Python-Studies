6c4d2ef21fa5f195a6d8e62c4aa0bb1bcf6de2a0
nirglus <nirgluskin@gmail.com>
2023-10-29 11:23:05 +0200

    add task

--- basics.py
+++ basics.py
@@ -1131,3 +1131,37 @@ print(vague_list[2])
 print(vague_list[2])
 # We have overridden the len() function for the class VagueList to return a random number.
 # The indexing function also returns a random item in a range from the list, based on the expression.
+
+# ----------- Task -----------
+"""
+We are improving our drawing application.
+Our application needs to support adding and comparing two Shape objects.
+Add the corresponding methods to enable addition + and comparison using the greater than > operator
+ for the Shape class.
+
+The addition should return a new object with the sum of the widths and heights of the operands, 
+while the comparison should return the result of comparing the areas of the objects.
+
+The given code creates two Shape objects from user input, outputs the area() of their addition and compares them.
+"""
+class Shape: 
+    def __init__(self, w, h):
+        self.width = w
+        self.height = h
+
+    def area(self):
+        return self.width*self.height
+
+    #your code goes here
+    
+w1 = int(input())
+h1 = int(input())
+w2 = int(input())
+h2 = int(input())
+
+s1 = Shape(w1, h1)
+s2 = Shape(w2, h2)
+result = s1 + s2
+
+print(result.area())
+print(s1 > s2)
