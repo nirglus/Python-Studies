1eb4d5045d96d87929dc1c5b5b4fd469799e5977
nirglus <nirgluskin@gmail.com>
2023-11-03 00:27:34 +0200

    add task

--- basics.py
+++ basics.py
@@ -1509,3 +1509,37 @@ except ZeroDivisionError:
 else:
    print(5)
 #! prints only 4 (only except)
+
+#! ----------- Raising Exceptions -----------
+"""
+You can throw (or raise) exceptions when some condition occurs.
+For example, when you take user input that needs to be in a specific format, you can throw an exception when it does not meet the requirements.
+This is done using the raise statement.
+"""
+num = 102
+if num > 100:
+  raise ValueError
+
+# NOTE You need to specify the type of the exception raised. In the code above, we raise a ValueError.
+
+# Exceptions can be raised with arguments that give detail about them.
+  name = "123"
+  raise NameError("Invalid name!") # returns "NameError: Invalid name!"
+
+# This makes it easier for other developers to understand why you raised the exception.
+
+# ----------- Task -----------
+"""
+You are making a program to post tweets. Each tweet must not exceed 42 characters.
+Complete the program to raise an exception, in case the length of the tweet is more than 42 characters.
+"""
+# You can use the len() function to calculate the length of the string.
+tweet = input()
+
+try:
+    #your code goes here
+    
+except:
+    print("Error")
+else:
+    print("Posted")
