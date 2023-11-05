7282d1598fd5e56c32c756fb264c6747c1affad1
nirglus <nirgluskin@gmail.com>
2023-11-05 15:27:23 +0200

    add task

--- basics.py
+++ basics.py
@@ -1663,3 +1663,18 @@ with open("/usercode/files/books.txt") as f:
 
 # The file is automatically closed at the end of the with statement, even if exceptions occur within it.
 
+# ----------- Task -----------
+"""
+You are given a books.txt file, which includes book titles, each on a separate line.
+Create a program to output how many words each title contains, in the following format:
+Line 1: 3 words
+Line 2: 5 words
+...
+
+Make sure to match the above mentioned format in the output.
+
+To count the number of words in a given string, you can use the split() function, or, alternatively, 
+count the number of spaces (for example, if a string contains 2 spaces, then it contains 3 words).
+"""
+with open("/usercode/files/books.txt") as f:
+   #your code goes here
